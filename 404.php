fb8598b0e838b6ad5b48c8a4a2386c8980746ef5
Andrew Fleming <66144906+AndrewJFleming@users.noreply.github.com>
2021-07-09 15:33:54 -0700

    Added 404.php file

---
+++ 404.php
@@ -0,0 +1,31 @@
+<?php
+    get_header();
+?>
+
+<p class="pageTypeHint">404.php</p>
+
+    <div class="page-wrap">
+        <div class="container">
+
+            <h1 class="text-center mt-5">404</h1>
+            
+                <div class="row">
+
+                    <!-- Contact Form 7 -->
+                    <div class="col-12">
+                        <h5 style="font-family: 'Optima', sans-serif" class="text-center">This page no longer exists...</h5>
+                        <p class="font-italic text-center">Please choose a link from the navigation menu above or<br/>
+                        return to the <a href="http://www.johnflemingartist.com/wp/">homepage</a>.</p>
+                    </div>
+                    
+                </div><!-- END First Row -->
+
+            </div><!-- END container -->
+    
+        </div><!-- END page-wrap -->  
+         
+    </div><!-- END main -->    
+
+<?php
+    get_footer();
+?>
